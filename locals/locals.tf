08ef7277600e5165a4817b3f80165a3d235f5ba7
unknown <saibabu663@gmail.com>
2024-05-23 18:44:55 +0530

    functions

---
+++ locals/locals.tf
@@ -0,0 +1,10 @@
+locals {
+
+  ami_id = "ami-090252cbe067a9e58"
+  sg_id = "sg-078f4ac08dcebe778" # replace with your SG ID
+  #instance_type = "t3.micro"
+  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
+  tags = {
+    Name = "locals"
+  }
+}
